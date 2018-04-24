2b03041da696dd176ba2058b2cb60ce50cd48ddd
Muhammet Soysal <msoysal92@gmail.com>
2018-04-24 23:55:27 +0300

    Initial commit

---
+++ core-commons/src/main/java/com/soysal/core/commons/App.java
@@ -0,0 +1,13 @@
+package com.soysal.core.commons;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ core-services/src/main/java/com/msoysal/core/services/App.java
@@ -0,0 +1,13 @@
+package com.msoysal.core.services;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
